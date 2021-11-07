ef3190291018a5a0a5722ec82c6218680e51c00e
bemo <mohamed.bennis@octo.com>
2021-11-07 17:00:47 +0100

    [step 4] create and implement use case

---
+++ use-cases/src/test/java/com/octo/usecases/CityForecastInteractorTest.java
@@ -0,0 +1,10 @@
+package com.octo.usecases;
+
+import org.junit.Test;
+
+public class CityForecastInteractorTest {
+    @Test
+    public void canCreateInteractor() {
+        final CityForecastInteractor interactor = new CityForecastInteractor();
+    }
+}
