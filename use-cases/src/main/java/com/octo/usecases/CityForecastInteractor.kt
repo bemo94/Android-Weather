89fbee3dbbc0759a041685088f101ae27fb6fbe4
bemo <mohamed.bennis@octo.com>
2021-11-16 01:11:31 +0100

    [03] start useCase

---
+++ use-cases/src/main/java/com/octo/usecases/CityForecastInteractor.kt
@@ -0,0 +1,5 @@
+package com.octo.usecases
+
+class CityForecastInteractor {
+
+}
---
+++ use-cases/src/test/java/com/octo/usecases/CityForecastInteractorTest.kt
@@ -0,0 +1,10 @@
+package com.octo.usecases
+
+import org.junit.Test
+
+class CityForecastInteractorTest {
+    @Test
+    fun canCreateInteractor() {
+        val interactor = CityForecastInteractor()
+    }
+}
